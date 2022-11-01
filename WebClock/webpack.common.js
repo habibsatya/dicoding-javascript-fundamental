6df1203d9b84006070693ed7c6a7c9eb854e27da
habibsatya <satyawiguna102@gmail.com>
2022-11-01 09:47:20 +0800

    merge webpack config

--- WebClock/webpack.common.js
+++ WebClock/webpack.common.js
@@ -4,38 +4,28 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     entry: './src/index.js',
     output: {
+        filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
     },
-    mode: 'production',
     module: {
         rules: [
+            /* style and css loader */
             {
                 test: /\.css$/,
                 use: [
                     {
-                        loader: 'style-loader'
+                        loader: 'style-loader',
                     },
                     {
-                        loader: 'css-loader'
+                        loader: 'css-loader',
                     },
-                ]
+                ],
             },
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: [
-                    {
-                        loader: 'babel-loader',
-                        options: {
-                            presets: ['@babel/preset-env']
-                        }
-                    }
-                ]
-            }
-        ]
+        ],
     },
+    /* plugin */
     plugins: [
+        /* HTML Webpack Plugin */
         new HtmlWebpackPlugin({
             template: './src/template.html',
             filename: 'index.html',
