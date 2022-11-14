ecec686deb29c1a943180ee7fbbd8ce22f3bfc38
habibsatya <satyawiguna102@gmail.com>
2022-11-14 15:36:10 +0800

    webpack added to the-c19

---
+++ The-C19/src/index.js
@@ -0,0 +1,7 @@
+import "regenerator-runtime";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles/style.css";
+import "./scripts/component/logo-web.js";
+import main from "./scripts/index.js";
+
+main();
